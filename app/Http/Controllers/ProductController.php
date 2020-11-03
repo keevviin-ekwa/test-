b0c5e6c5abd3d797b9ddac8ec6c50fec4c09210b
kevn ekwa <ekwabayard@gmail.com>
2020-11-03 16:01:13 +0100

    second commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,7 +8,16 @@ use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
+
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
+     *
+     *
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
